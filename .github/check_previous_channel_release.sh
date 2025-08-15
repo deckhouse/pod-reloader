502224a5463a7a5ab068561865bff055c2688ee0
Maxim Smyslov <70270470+riptide-01@users.noreply.github.com>
2025-08-15 16:08:57 +0300

    fix check previous release (#9)
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>
    Co-authored-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- .github/check_previous_channel_release.sh
+++ .github/check_previous_channel_release.sh
@@ -28,7 +28,7 @@ fi
 
 echo "Checking previous channel $previousChannel"
 $crane auth login -u $user -p $password registry.deckhouse.io
-previousChannelVersion=$($crane export registry.deckhouse.io/deckhouse/$edition/modules/$repositoryName/release:$previousChannel | grep -aoE '\{"version":".*"\}' | jq -r .version)
+previousChannelVersion=$($crane export registry.deckhouse.io/deckhouse/$edition/modules/$repositoryName/release:$previousChannel | tar -xOf - version.json | jq -r .version)
 if [[ "$version" == "$previousChannelVersion" ]]; then
  echo "Previous channel $previousChannel version $previousChannelVersion is equal desired version $version, processing"
  exit 0
